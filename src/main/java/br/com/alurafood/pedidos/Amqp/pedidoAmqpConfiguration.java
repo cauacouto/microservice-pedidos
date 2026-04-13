0556b8ba112c006067694b3af4504dd0004cb2eb
cauacouto <cauacouto022@gmail.com>
2026-04-12 21:15:52 -0300

    feat:criar pedidoAmqpConfiguration

---
+++ src/main/java/br/com/alurafood/pedidos/Amqp/pedidoAmqpConfiguration.java
@@ -0,0 +1,25 @@
+package br.com.alurafood.pedidos.Amqp;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class pedidoAmqpConfiguration {
+
+
+    @Bean
+    public JacksonJsonMessageConverter messageConverter(){
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate (ConnectionFactory connectionFactory, JacksonJsonMessageConverter messageConverter){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+}
+
