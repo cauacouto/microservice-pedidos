8535b04510df09eb856d89e0baf901e7608dbaf3
cauacouto <cauacouto022@gmail.com>
2026-04-13 21:56:07 -0300

    feat: criar nova fila

--- src/main/java/br/com/alurafood/pedidos/Amqp/PagamentoListener.java
+++ src/main/java/br/com/alurafood/pedidos/Amqp/PagamentoListener.java
@@ -2,17 +2,18 @@ package br.com.alurafood.pedidos.Amqp;
 
 import br.com.alurafood.pedidos.dto.PagamentoDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PagamentoListener {
 
 
-    @RabbitListener(queues = "pagamento.concluido")
-    public void recebeMesagem(PagamentoDto pagamento){
+    @RabbitListener(queues = "pagamentos.detalhes-pedido")
+    public void recebeMesagem(@Payload PagamentoDto pagamento){
 
         String mensagem = """
-                   Dados do pagamento: %s
+           Dados do pagamento: %s
            Número do pedido: %s
            Valor R$: %s
            Status: %s 
